049ca2a0843cdeeb11a5a9b938a42e914b076986
Artem <prytkinart@mail.ru>
2020-03-23 18:28:13 +0400

    добавил переключение экранов

--- app/js/main.js
+++ app/js/main.js
@@ -98,7 +98,125 @@ if ($slider.length) {
 
   $slider.slick({
     fade: true,
-    prevArrow: '.arrow-back',
-    nextArrow: '.arrow-forward',
+    prevArrow: '.arrow-back.cart',
+    nextArrow: '.arrow-forward.cart',
   });
 }
+
+var $slider = $('.cart__slider2');
+
+if ($slider.length) {
+  var currentSlide;
+  var slidesCount;
+  var sliderCounter = document.createElement('div');
+  sliderCounter.classList.add('slider__counter');
+  
+  var updateSliderCounter = function(slick, currentIndex) {
+    currentSlide = slick.slickCurrentSlide() + 1;
+    slidesCount = slick.slideCount;
+    sliderCounter.innerHTML = '<p class="slider-current">' + currentSlide + '</p>' + '<p class="slider-slash">' + '/' + '</p>' + '<p class="slider-count">' + slidesCount + '</p>'
+  };
+
+  $slider.on('init', function(event, slick) {
+    $slider.append(sliderCounter);
+    updateSliderCounter(slick);
+  });
+
+  $slider.on('afterChange', function(event, slick, currentSlide) {
+    updateSliderCounter(slick, currentSlide);
+  });
+
+  $slider.slick({
+    fade: true,
+    prevArrow: '.arrow-back.cart2',
+    nextArrow: '.arrow-forward.cart2',
+  });
+}
+
+const slider = $('.index_page-container');
+const carret = $('.carret');
+const heightSlider = slider.height();
+const countPages = 3; // считать от нуля
+const loop = false; //зациклить
+const animateDuration = 300;
+const fullAnimate = animateDuration * countPages / 3;
+const heightStep = heightSlider / countPages;
+var indexPage = parseInt( $('.page.active').attr('index') );
+
+function onScrolling() {
+  $(window).on('mousewheel DOMMouseScroll MozMousePixelScroll', scrolling);
+}
+function offScrolling() {
+  $(window).off('mousewheel DOMMouseScroll MozMousePixelScroll', scrolling);
+}
+
+function scrolling (e) {
+   let delta = parseInt(e.originalEvent.wheelDelta || -e.originalEvent.detail);
+    if (delta >= 0) {
+      if ( (indexPage > 0 && !loop) || loop ) {
+        offScrolling();
+        indexPage--;
+        let trueAnimate = animateDuration;
+        
+        if (loop && indexPage < 0) {
+          indexPage = countPages;
+          trueAnimate = fullAnimate;
+        }
+        
+        $('.page.active, button.index.active').removeClass('active');
+        $('.page[index="'+indexPage+'"], button.index[index="'+indexPage+'"]').addClass('active');
+        let carretTop = indexPage * heightStep;
+        carret.animate({'top': carretTop + 'px'}, trueAnimate, function() {
+          onScrolling();
+        });
+      }
+    } else {
+      if ( (indexPage < countPages && !loop) || loop  ) {
+        offScrolling();
+        indexPage++;
+        let trueAnimate = animateDuration;
+        
+        if (loop && indexPage > countPages) {
+          indexPage = 0;
+          trueAnimate = fullAnimate;
+        }
+          
+        $('.page.active, button.index.active').removeClass('active');
+        $('.page[index="'+indexPage+'"], button.index[index="'+indexPage+'"]').addClass('active');
+        let carretTop = indexPage * heightStep;
+        carret.animate({'top': carretTop + 'px'}, trueAnimate,  function() {
+          onScrolling();
+        });
+      }
+    }
+  }
+
+$(document).ready(function() {
+  $(window).on('mousewheel DOMMouseScroll MozMousePixelScroll', scrolling);
+});
+
+$('button.index').on('click', function() {
+  if( !$(this).is('.active') ) {
+    
+    offScrolling();
+    let trueAnimate = animateDuration;
+    let thisIndex = parseInt( $(this).attr('index') );
+    
+    if ( (indexPage === 0 && thisIndex === countPages) || 
+        (indexPage === countPages && thisIndex === 0) || 
+        ( (indexPage - thisIndex) > 1 ) ||
+        ( (indexPage - thisIndex) < -1 ) ) {
+      trueAnimate = fullAnimate;
+    }
+      
+    
+    indexPage = thisIndex;
+    
+    $('.page.active, button.index.active').removeClass('active');
+    $('.page[index="'+indexPage+'"], button.index[index="'+indexPage+'"]').addClass('active');
+    let carretTop = indexPage * heightStep;
+    carret.animate({'top': carretTop + 'px'}, trueAnimate,  function() {
+      onScrolling();
+    });
+  }
+});
